94d05c366835fd9fd37998760146a4797092f23b
ills5771 <ilyayelizarov1@gmail.com>
2019-03-02 14:37:17 -0800

    added todoReducer

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+  todos: [
+    { task: "Rake leaves", completed: false },
+    { task: "Laundry", completed: false }
+  ]
+};
+
+export const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      const newTodo = {
+        task: action.payload,
+        completed: false
+      };
+      return {
+        ...state,
+        todos: [...state.todos, newTodo]
+      };
+    default:
+      return state;
+  }
+};
