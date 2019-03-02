1f28c2a7f9bde161edd14f2ac48b460186879bf8
ills5771 <ilyayelizarov1@gmail.com>
2019-03-02 15:15:56 -0800

    added addNewTodo in actions

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-
+import TodoList from "./containers/TodoList";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <TodoList/>
-<TodoForm/> */}
+        <TodoList />
         Redux Todo
       </div>
     );
